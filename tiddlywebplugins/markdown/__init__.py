a0c239e3a9a700757b1dd38620d1a6a5e33b3a8d
Chris Dent <chris.dent@gmail.com>
2013-05-09 12:18:58 +0100

    Release 1.0.4

--- tiddlywebplugins/markdown/__init__.py
+++ tiddlywebplugins/markdown/__init__.py
@@ -49,7 +49,7 @@ then set
  'wikitext.default_renderer': 'tiddlywebplugins.markdown'
 """
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 
 # for sake of making config calls clean, we import render
 # into this space
