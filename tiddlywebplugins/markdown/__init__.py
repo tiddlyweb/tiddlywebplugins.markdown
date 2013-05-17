21c14230a0716c6537f90b00ff519e72a5684618
Chris Dent <chris.dent@gmail.com>
2013-05-17 15:17:23 +0100

    release 1.0.5
    * improved ordering of parsing rules

--- tiddlywebplugins/markdown/__init__.py
+++ tiddlywebplugins/markdown/__init__.py
@@ -49,7 +49,7 @@ then set
  'wikitext.default_renderer': 'tiddlywebplugins.markdown'
 """
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 # for sake of making config calls clean, we import render
 # into this space
