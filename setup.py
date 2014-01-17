96b20d4de1349510674f021ed73be5f2baeb3490
Chris Dent <chris.dent@gmail.com>
2014-01-17 23:22:01 +0000

    Update license, change version number.

--- setup.py
+++ setup.py
@@ -3,25 +3,22 @@ AUTHOR = 'Chris Dent'
 AUTHOR_EMAIL = 'cdent@peermore.com'
 NAME = 'tiddlywebplugins.markdown'
 DESCRIPTION = 'Markdown rendering for TiddlyWeb'
-VERSION = '1.1.0'
+VERSION = '1.2.0'
 
 import os
 from setuptools import setup, find_packages
 
 setup(
-        namespace_packages = ['tiddlywebplugins'],
-        name = NAME,
-        version = VERSION,
-        description = DESCRIPTION,
-        long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
-        author = AUTHOR,
-        url = 'http://pypi.python.org/pypi/%s' % NAME,
-        packages = find_packages(exclude=['test']),
-        author_email = AUTHOR_EMAIL,
-        platforms = 'Posix; MacOS X; Windows',
-        install_requires = ['tiddlyweb', 'Markdown'],
-        extras_require = {
-            'tiddlyspace':  ['tiddlywebplugins.tiddlyspace']
-        },
-        zip_safe = False,
-        )
+    namespace_packages = ['tiddlywebplugins'],
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    author = AUTHOR,
+    url = 'http://pypi.python.org/pypi/%s' % NAME,
+    packages = find_packages(exclude=['test']),
+    author_email = AUTHOR_EMAIL,
+    platforms = 'Posix; MacOS X; Windows',
+    install_requires = ['tiddlyweb', 'Markdown'],
+    zip_safe = False,
+)
